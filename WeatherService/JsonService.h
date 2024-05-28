9530fb94832408f844f3d211fb5d55d8fb47ff26
LirikDer <kir40254@gmail.com>
2024-05-28 22:06:37 +0300

    Добавил класс для ввода данных из json

---
+++ WeatherService/JsonService.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <fstream>
+#include "Weather.h"
+#include "Service.h"
+#include "json.hpp"
+class JsonService :public Service
+{
+public:
+	virtual Weather getWeather(std::string s) override;
+	virtual ~JsonService() {};
+};
